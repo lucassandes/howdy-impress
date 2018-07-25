4f21efd4dcf5e8ec23db07276f2efa5f6825bcc4
lucassandes <lucassandes@msn.com>
2018-07-24 23:24:02 -0300

    Encapsulated App in an IIFE

--- js/app.js
+++ js/app.js
@@ -1,98 +1,104 @@
-"use strict";
+(function IIFE() {
+  "use strict";
 
-const API_URL = "https://www.drukzo.nl.joao.hlop.nl/challenge.php";
-const $ = document.getElementById.bind(document);
 
-function makeGuess(player) {
-  const guessElement = $('input-' + player);
-  const guess = guessElement.value;
-  const params = {
-    method: 'GET'
-  };
+  const API_URL = "https://www.drukzo.nl.joao.hlop.nl/challenge.php";
+  const $ = document.getElementById.bind(document);
 
-  console.log(`Make Guess player=${player} guess=${guess}`);
+  function makeGuess(player) {
+    const guessElement = $('input-' + player);
+    const guess = guessElement.value;
+    const params = {
+      method: 'GET'
+    };
 
-  if (guess < 0 || guess > 100) {
-    guessElement.classList.add("animated", "shake");
+    console.log(`Make Guess player=${player} guess=${guess}`);
+
+    if (guess < 0 || guess > 100) {
+      guessElement.classList.remove("animated", "shake");
+      guessElement.classList.add("animated", "shake");
+    } else {
+      fetch(`${API_URL}?player=${player}&guess=${guess}`, params)
+        .then((resp) => resp.json())
+        .then(function (data) {
+          console.log(`RESULT Guess player=${player} guess=${guess} = ${data.guess}`);
+
+          if (data.guess === "Bingo!!!" || guess == 42) {
+            toggleAllButtons();
+            showWinner(player, guess);
+          }
+
+        }).catch(function (error) {
+          console.log(error);
+        });
+    }
   }
 
+  function createPlayers(players) {
+    const playerContainer = $('player-container');
 
-  else {
-    fetch(`${API_URL}?player=${player}&guess=${guess}`, params)
-      .then((resp) => resp.json())
-      .then(function (data) {
-        console.log(`RESULT Guess player=${player} guess=${guess} = ${data.guess}`);
-
-        if (data.guess === "Bingo!!!" || guess == 42) {
-          toggleAllButtons();
-          showWinner(player, guess);
-        }
-
-      }).catch(function (error) {
-        console.log(error);
-      });
-  }
-}
-
-function createPlayers(players) {
-  const playerContainer = $('player-container');
-
-  for (let pl of players) {
-    const element =
-      `<div class="player" id="player-${pl.id}">
+    for (let pl of players) {
+      const element =
+        `<div class="player" id="player-${pl.id}">
           <div class="instruction">Enter a number between 0 and 100</div>
           <input type="number" id="input-${pl.id}" class="input"  />
           <button class="button" onclick="makeGuess(${pl.id})">Submit</button>
       </div>`;
-    playerContainer.insertAdjacentHTML("beforeend", element);
+      playerContainer.insertAdjacentHTML("beforeend", element);
+    }
   }
-}
 
-function showWinner(player, guess) {
-  const appContainer = $('app');
+  function showWinner(player, guess) {
+    const appContainer = $('app');
 
-  const element =
-    `<div class="winner" id="winner">
+    const element =
+      `<div class="winner" id="winner">
       <div>Bingo!!!</div>
       <div>Congratulations Player ${player}, you guessed it right!</div>
       <div>The right number is: ${guess}</div>
       <button onclick="restartGame()">Restart</button>
     </div>`;
 
-  appContainer.insertAdjacentHTML("afterbegin", element);
-}
-
-function restartGame() {
-  toggleAllButtons();
-
-  const winnerContainer = $('winner');
-  winnerContainer.remove();
-
-  const inputs = document.getElementsByClassName('input');
-  for (let inp of inputs) {
-    inp.value = "";
+    appContainer.insertAdjacentHTML("afterbegin", element);
   }
-}
 
-function toggleAllButtons() {
-  const buttons = document.getElementsByClassName('button');
-  for (let btn of buttons) {
-    btn.disabled = !btn.disabled;
+  function restartGame() {
+    toggleAllButtons();
+
+    const winnerContainer = $('winner');
+    winnerContainer.remove();
+
+    const inputs = document.getElementsByClassName('input');
+    for (let inp of inputs) {
+      inp.value = "";
+    }
   }
-}
 
-let players = [{
-    id: 1,
-    guess: null
-  },
-  {
-    id: 2,
-    guess: null
-  },
-  {
-    id: 3,
-    guess: null
-  },
-];
+  function toggleAllButtons() {
+    const buttons = document.getElementsByClassName('button');
+    for (let btn of buttons) {
+      btn.disabled = !btn.disabled;
+    }
+  }
 
-createPlayers(players);
+  let players = [{
+      id: 1,
+      guess: null
+    },
+    {
+      id: 2,
+      guess: null
+    },
+    {
+      id: 3,
+      guess: null
+    },
+  ];
+
+  createPlayers(players);
+})();
+// var app = (function IIFE(){
+//   alert('HUBESE!');
+//   const testee = function() {
+//     alert('HUE!');
+//   };
